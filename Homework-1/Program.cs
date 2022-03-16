80f44be7f240bb352dfaa885cceee2605a6530f8
Hüseyn Alıyev <huseynma@code.edu.az>
2022-03-16 18:52:12 +0400

    Created C#-project

---
+++ Homework-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 254324356;
+            int sum = 0;
+
+            while (number != 0)
+            {
+                number -= number % 10;
+                number /= 10;
+                sum++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
